8b11c96247d9ee7e634fb504fceadddc1ca137e9
MgrGracz <kamil.bandzwolek@gmail.com>
2024-03-19 22:39:09 +0100

    v002

--- multiSol.js
+++ multiSol.js
@@ -2,30 +2,53 @@ require('dotenv').config();
 const fs = require('fs');
 const parse = require('csv-parser');
 const { Connection, PublicKey, Transaction, SystemProgram, Keypair, sendAndConfirmTransaction } = require('@solana/web3.js');
+const { getOrCreateAssociatedTokenAccount, createTransferInstruction} = require ('@solana/spl-token');
 const bs58 = require("bs58");
 
 
 // Tu zmieniamy  czy to ma byc main czy test
-const connection = new Connection('https://api.testnet.solana.com');    //test https://api.testnet.solana.com   main  https://api.mainnet-beta.solana.com
+const connection = new Connection('https://api.mainnet-beta.solana.com');    //test https://api.testnet.solana.com   main  
 
 
 fs.createReadStream('wallets.csv')
   .pipe(parse())
   .on('data', async (row) => {
     const recipient = new PublicKey(row.wallet);
-    const valueToSend = row.value;
-    console.log(process.env.PRIVATE_KEY)
+    const valueToSend  = parseInt(row.value);
     const privateKey = new Uint8Array(bs58.decode(process.env.PRIVATE_KEY));
     const account = Keypair.fromSecretKey(privateKey);
+    const mintAddres=new PublicKey(process.env.MINT_ADDRESS);
+   
+
+    // Creates or fetches the associated token accounts for the sender and receiver.
+    let sourceAccount = await getOrCreateAssociatedTokenAccount(
+        connection,
+        account,
+        mintAddres,
+        account.publicKey
+      );
+
+      console.log(sourceAccount)
+
+  
+
+    let destinationAccount = await getOrCreateAssociatedTokenAccount(
+        connection,
+        account,
+        mintAddres,
+        recipient
+      );
+
 
     const transaction = new Transaction().add(
-      SystemProgram.transfer({
-        fromPubkey: account.publicKey,
-        toPubkey: recipient,
-        lamports: valueToSend * 1000000000, // Wartość w lamportach
-      })
+        splToken.createTransferInstruction(
+            sourceAccount.address,
+            destinationAccount.address,
+            account.publicKey,
+            valueToSend,
+          )
     );
-
+        console.log(transaction)
     // Podpisz i wyślij transakcję
     const signature = await sendAndConfirmTransaction(
       connection,
@@ -33,8 +56,13 @@ fs.createReadStream('wallets.csv')
       [account] // Podpisz transakcję prywatnym kluczem nadawcy
     );
 
+
+
+
     console.log('Transakcja wysłana:', signature);
   })
   .on('end', () => {
     console.log('Zakończono przetwarzanie pliku CSV. Oczekiwanie na potwierdzenia transakcji.');
   });
+
+
