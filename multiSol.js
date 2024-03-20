505977a675da20b8b7e8e6bcdfb7160518276be0
MgrGracz <kamil.bandzwolek@gmail.com>
2024-03-20 21:20:08 +0100

    v003
    a

--- multiSol.js
+++ multiSol.js
@@ -1,13 +1,13 @@
 require('dotenv').config();
 const fs = require('fs');
 const parse = require('csv-parser');
-const { Connection, PublicKey, Transaction, SystemProgram, Keypair, sendAndConfirmTransaction } = require('@solana/web3.js');
-const { getOrCreateAssociatedTokenAccount, createTransferInstruction} = require ('@solana/spl-token');
+const { Connection, PublicKey, Transaction, SystemProgram, Keypair, sendAndConfirmTransaction, clusterApiUrl } = require('@solana/web3.js');
+const {createTransferInstruction} = require ('@solana/spl-token');
 const bs58 = require("bs58");
 
 
 // Tu zmieniamy  czy to ma byc main czy test
-const connection = new Connection('https://api.mainnet-beta.solana.com');    //test https://api.testnet.solana.com   main  
+const connection = new Connection("https://api.devnet.solana.com "); // dev https://api.devnet.solana.com test https://api.testnet.solana.com   main  
 
 
 fs.createReadStream('wallets.csv')
@@ -17,47 +17,27 @@ fs.createReadStream('wallets.csv')
     const valueToSend  = parseInt(row.value);
     const privateKey = new Uint8Array(bs58.decode(process.env.PRIVATE_KEY));
     const account = Keypair.fromSecretKey(privateKey);
-    const mintAddres=new PublicKey(process.env.MINT_ADDRESS);
-   
+    const mintAddress=new PublicKey(process.env.MINT_ADDRESS);
+    const tokenProgram=new PublicKey(process.env.TOKEN_PROGRAM);
 
-    // Creates or fetches the associated token accounts for the sender and receiver.
-    let sourceAccount = await getOrCreateAssociatedTokenAccount(
-        connection,
-        account,
-        mintAddres,
-        account.publicKey
+    // Utwórz instrukcję transferu
+    const transferInstruction = createTransferInstruction(
+        tokenProgram,
+        mintAddress,
+        account.publicKey,
+        valueToSend,
+        recipient,
       );
 
-      console.log(sourceAccount)
+   // Utwórz transakcję i dodaj instrukcję transferu
+   const transaction = new Transaction().add(transferInstruction);
 
-  
-
-    let destinationAccount = await getOrCreateAssociatedTokenAccount(
-        connection,
-        account,
-        mintAddres,
-        recipient
-      );
-
-
-    const transaction = new Transaction().add(
-        splToken.createTransferInstruction(
-            sourceAccount.address,
-            destinationAccount.address,
-            account.publicKey,
-            valueToSend,
-          )
-    );
-        console.log(transaction)
-    // Podpisz i wyślij transakcję
-    const signature = await sendAndConfirmTransaction(
-      connection,
-      transaction,
-      [account] // Podpisz transakcję prywatnym kluczem nadawcy
-    );
-
-
-
+   // Podpisz i wyślij transakcję
+   const signature = await sendAndConfirmTransaction(
+     connection,
+     transaction,
+     [account]
+   );
 
     console.log('Transakcja wysłana:', signature);
   })
